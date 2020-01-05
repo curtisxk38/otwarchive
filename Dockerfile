fb99a4108da54419cb0549cb5e8f2cbda45f7c42
curtisxk38 <ck2ha@virginia.edu>
2020-01-05 17:07:17 -0500

    Check if mysql server is ready for connections before running rake commands

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ruby:2.6.5
-RUN apt-get update && apt-get install -y default-mysql-client
+RUN apt-get update && apt-get install -y default-mysql-client netcat
 
 WORKDIR /otwa
 COPY Gemfile .
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,47 @@
 #!/usr/bin/env bash
+set -e
 
 rm -f tmp/pids/server.pid
+
+################# 
+# Wait for mysql server to respond to netcat ping before running rake commands
+################# 
+
+export SLEEP_TIME=5
+
+export TIME_OUT=90s
+
+wait_for()
+{
+    while :
+    do
+        nc -z db 3306
+        WAITFORIT_result=$?
+        if [[ $WAITFORIT_result -eq 0 ]]; then
+            echo "mysql is ready"
+            break
+        fi
+        echo "mysql not ready, sleeping"
+        sleep $SLEEP_TIME
+    done
+    return $WAITFORIT_result
+}
+
+export -f wait_for
+
+trap 'kill -INT -$pid' INT
+timeout $TIME_OUT bash -c wait_for &
+pid=$!
+wait $pid
+
+TO_result=$?
+
+if [[ $TO_result -eq 124 ]]; then
+	echo "Failed to connect after $TIME_OUT"
+	exit $TO_result
+fi
+
+##################################
 
 rake db:create
 rake db:schema:load
@@ -14,6 +55,8 @@ rake search:index_tags
 rake search:index_works
 rake search:index_pseuds
 rake search:index_bookmarks
+
+sleep 60
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile 
 #  or as command in docker-compose.yml)
